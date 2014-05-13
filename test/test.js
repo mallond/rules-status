7c963f6dfc8c83fd988f11f0fde03e781709dba8
David Mallon <mallond@gmail.com>
2014-05-13 10:57:15 -0400

    x

---
+++ test/test.js
@@ -0,0 +1,83 @@
+//     http://www.bizrez.com
+//     (c) 2004-2014 David Mallon
+//     Freely distributed under the MIT license.
+
+/*global describe: false, it: false*/
+
+var assert = require("assert");
+
+
+
+
+(function () {
+
+    "use strict";
+
+    describe('Test Suite  ', function () {
+
+        describe('Token Test', function () {
+
+            it('should return token', function () {
+
+                assert.strictEqual(true, true);
+
+            });
+        });
+
+        describe('Create Test', function () {
+
+            it('should return mongo id', function () {
+
+                assert.strictEqual(true, true);
+
+            });
+        });
+
+        describe('Read Test', function () {
+
+            it('should return {ok:1, json_result}', function () {
+
+                assert.strictEqual(true, true);
+
+            });
+        });
+
+        describe('Update Test', function () {
+
+            it('should return {ok:1}', function () {
+
+                assert.strictEqual(true, true);
+
+            });
+        });
+
+        describe('Delete Test', function () {
+
+            it('should return {ok:1}', function () {
+
+                assert.strictEqual(true, true);
+
+            });
+        });
+
+        describe('List Test', function () {
+
+            it('should return {ok:1, JsonList}', function () {
+
+                assert.strictEqual(true, true);
+
+            });
+        });
+
+
+
+
+
+
+
+
+    });
+
+
+
+})();
